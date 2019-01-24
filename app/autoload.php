ede95ae8e456eba9dc4c1ab4208d9a46acb2d699
Abraham Mamani <abraham.e.mamani@gmail.com>
2019-01-24 19:36:41 -0300

    [Inserción y configuración básica de archivo database/config.php][Modificación en archivos index.php y autoload.php]

--- app/autoload.php
+++ app/autoload.php
@@ -2,5 +2,5 @@
     spl_autoload_register(function ($className){
         require_once 'System/'.$className.'.php';
     });
-    //require_once '../../config.php';
+    require_once '../database/config.php';
 ?>
---
+++ database/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('__DB_CONNECTION__'  , 'mysql');
+    define('__DB_HOST__'        , 'localhost');
+    define('__DB_DATABASE__'    , '');
+    define('__DB_USERNAME__'    , 'root');
+    define('__DB_PASSWORD__'    , '');
+?>
